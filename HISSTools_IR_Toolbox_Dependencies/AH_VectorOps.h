4df0a752aac6733644843e81e6f5cd4d232cee96
Alex Harker <ajharker@gmail.com>
2017-12-21 17:21:20 +0000

    Fixed conflict with SSE4.1 or later

--- HISSTools_IR_Toolbox_Dependencies/AH_VectorOps.h
+++ HISSTools_IR_Toolbox_Dependencies/AH_VectorOps.h
@@ -225,7 +225,7 @@ static const vDouble Vec_Ops_F64_One = {1.,1.};
 // These routines are taken directly from the apple SSE migration guide
 // The guide can be found at http://developer.apple.com/legacy/mac/library/documentation/Performance/Conceptual/Accelerate_sse_migration/Accelerate_sse_migration.pdf
 
-#ifdef __APPLE__
+#if defined(__APPLE__) && (!defined(__SSE4_1__))
 static __inline vSInt32 _mm_min_epi32(vSInt32 a, vSInt32 b) FORCE_INLINE;
 static __inline vSInt32 _mm_min_epi32(vSInt32 a, vSInt32 b) 
 { 
