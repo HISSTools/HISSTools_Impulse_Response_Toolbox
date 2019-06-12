782b8b99587971e82451b4e0cf5d7450c4344441
Alex Harker <ajharker@gmail.com>
2019-06-12 18:27:35 +0100

    Remove unneeded functions/defines

--- HISSTools_IR_Toolbox_Dependencies/AH_Math.h
+++ HISSTools_IR_Toolbox_Dependencies/AH_Math.h
@@ -6,8 +6,8 @@
  *
  */
 
-#ifndef _AH_WIN_MATH_
-#define _AH_WIN_MATH_
+#ifndef _AH_MATH_
+#define _AH_MATH_
 
 #ifndef __APPLE__
 
@@ -15,10 +15,6 @@
 
 #include <math.h>
 #include <float.h>
-
-#ifndef isfinite
-#define isfinite _finite
-#endif
 
 #ifdef isnan
 #undef isnan
@@ -28,21 +24,9 @@
 #undef isinf
 #endif
 
-#define LOG_2_RECIP 1.44269504088896340735992468100189213742664595415299
-
 static __inline double round(double r)
 {
     return (r > 0.0) ? floor(r + 0.5) : ceil(r - 0.5);
-}
-
-static __inline double log2(double n)
-{
-    return log(n) * LOG_2_RECIP;
-}
-
-static __inline double exp2(double n)
-{
-    return pow(2, n);
 }
 
 static __inline long isnan(double n)
@@ -55,51 +39,6 @@ static __inline long isinf(double n)
     return n == n * 2;
 }
 
-static __inline double copysign(double a, double b)
-{
-    double val = fabs(a);
-
-    return b < 0 ? -val : val;
-}
-
-static __inline double asinh(double x)
-{
-    return log(x + sqrt((x * x) + 1));
-}
-
-static __inline double acosh(double x)
-{
-    return log(x + sqrt((x * x) - 1));
-}
-
-static __inline double atanh(double x)
-{
-    return (log(1+x) - log(1-x))/2.0;
-}
-
-static __inline float asinhf(float x)
-{
-    return (float) asinh(x);
-}
-
-static __inline float acoshf(float x)
-{
-    return (float) acosh(x);
-}
-
-static __inline float atanhf(float x)
-{
-    return (float) atanh(x);
-}
-
-#ifndef FLT_MAX
-#define FLT_MAX 3.402823466e+38f
 #endif
 
-#ifndef DBL_MAX
-#define DBL_MAX 3.402823466e+38f
-#endif
-
-#endif
-
-#endif    /* _AH_WIN_MATH_ */
+#endif    /* _AH_MATH_ */
